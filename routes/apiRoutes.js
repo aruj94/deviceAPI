9886bf5123cf3330b916f326ababd5f3117a9971
aruj94 <37327782+aruj94@users.noreply.github.com>
2023-11-17 07:49:39 -0800

    Removed rate limiting logic from application. Now it is done on nginx server (#14)
    * connection to redis cloud instead of local redis
    
    * updating dev branch (#12)
    
    * Production to main (#11)
    
    * removed redundant code from prod branch
    
    * updates on readme
    
    * added descriptive comments and modified readme
    
    * update for float64 decimal issue
    
    * fix for negative temperatures
    
    * updating production branch to development
    
    * updates
    
    * jenkins pipeline test
    
    * changes to jenkinsfile
    
    * jenkinsfile test
    
    * jenkinsfile test
    
    * jenkinsfile test
    
    * jenkins file update
    
    * test
    
    * jenkins tesst
    
    * jenkins
    
    * jenkins
    
    * updates
    
    * push the image
    
    * image push
    
    * updates to jenkinsfile
    
    * jenkins update
    
    * jenkins updates
    
    * jenkins
    
    * jenkins update
    
    * update
    
    * update
    
    * j
    
    * fix
    
    * updates
    
    * update
    
    * envvar jenkins
    
    * .env file to be ignored from docker
    
    * jenkins gke cluster creation stage
    
    * jenkins update
    
    * jenkins update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * bat update
    
    * updates
    
    * update
    
    * jenkins update
    
    * jenkins update
    
    * jenkins file updates
    
    * update
    
    * update
    
    * updates to readme file
    
    * Automatic sync from mongodb to redis is changed. Now data is synced when the user sends a GET request. A time to live policy is implemented for API keys and device data in redis.
    
    * removed rate limit check logic from application layer. Now nginx reverse proxy server is used for rate limiting.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,21 @@
 import { Router } from "express";
 import { initialMessage, getErrorsCollectionData, deleteData, postErrorsData } from "../controllers/dataController.js";
 import { authentication } from "../middleware/authentication.js";
-import { checkRateLimit } from "../controllers/redisController.js";
 
 const router = Router();
 
 // Route to get the initial welcome message
-router.get('/', checkRateLimit, initialMessage)
+router.get('/', initialMessage)
 
 // Route to get all the error data saved in mongodb cluster
-router.get('/errors', authentication, checkRateLimit, getErrorsCollectionData )
+router.get('/errors', authentication, getErrorsCollectionData )
 
 // Route to delete all the error data from the mongodb collection
-router.delete('/errors', authentication, checkRateLimit, deleteData )
+router.delete('/errors', authentication, deleteData )
 
 // Route to test device temperature data and if the provided data
 // string is correctly formatted or not. If not formatted correctly 
 // then it is stored in mongoDb for future retreival.
-router.post('/temp', authentication, checkRateLimit, postErrorsData )
+router.post('/temp', authentication, postErrorsData )
 
 export default router;
